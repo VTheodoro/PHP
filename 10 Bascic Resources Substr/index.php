e9bfbe710da5dd430d27bc488c5e6d37c25a2e8c
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-04-25 14:28:28 -0300

    Update

---
+++ 10 Bascic Resources Substr/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula</title>
+</head>
+
+<body>
+
+    <?php
+
+    $texto = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita maxime iusto hic quisquam accusamus vel exercitationem ipsum fugit saepe.";
+echo substr($texo,5,9);
+//corta as 5 primeiras letras do texto e mostra apenas as proximas 9 a partir das que foram cortadas.
+    ?>
+
+</body>
+
+</html>
---
+++ 11 Basic Resources Strlen/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula</title>
+</head>
+
+<body>
+
+    <?php
+
+$texto="123456";
+
+//se a variavel texto não tiver nenhum caracter(letra) ele mostra a mensagem.
+if(strlen($texto) <1){
+    echo "O Campo Texto não pode ser vazio!";
+}
+
+    ?>
+
+</body>
+
+</html>
